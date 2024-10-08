f04f81b678f5f8ce5b2ff2b522c0a7fa974af550
jvanderlei <79838953+jvanderlei@users.noreply.github.com>
2024-10-08 16:07:37 -0300

    gRPC bi-way streaming tests

---
+++ Spike.gRPC.Server/Services/MessageService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+
+namespace Spike.gRPC.Server.Services
+{
+    public class MessageService : Comm.CommBase
+    {
+        private ILogger<MessageService> _logger;
+
+        public MessageService(ILogger<MessageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task BiWayStreaming(IAsyncStreamReader<ClientMessage> requestStream,
+            IServerStreamWriter<ServerMessage> responseStream, ServerCallContext context)
+        {
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    Console.WriteLine(message.ToString());
+                }
+            });
+
+            while (!readTask.IsCompleted)
+            {
+             
+                await responseStream.WriteAsync(new ServerMessage() { Message = "Hello World! pra não dar azar, mas salve"});
+                await Task.Delay(10000);
+
+            }
+        }
+    }
+}
---
+++ Spikes.gRPC.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+using Spike.gRPC.Server;
+
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7019/");
+var client = new Comm.CommClient(channel);
+using var call = client.BiWayStreaming();
+
+Console.WriteLine("Iniciando leitura de mensagens");
+
+var readTask = Task.Run(async () =>
+{
+    await foreach(var response in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(response.Message);
+    }
+});
+
+Console.WriteLine("Começando Stream de mensagens");
+while (true)
+{
+    var message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message))
+    {
+        break;
+    }
+
+    await call.RequestStream.WriteAsync(new ClientMessage { Message = message });
+}
+
+Console.WriteLine("Concluído");
+await call.RequestStream.CompleteAsync();
+await readTask;
